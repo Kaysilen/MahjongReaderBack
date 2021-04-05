6864a539a0048d7f6ba74a0fef0ae3e6c80a5318
오성돈 <sungdon_oj@tmax.co.kr>
2021-04-05 16:27:49 +0900

    [feat] analysisMahjongCard 구현

--- app.py
+++ app.py
@@ -16,10 +16,31 @@ def hello_world():
     result = calculator.estimate_hand_value(tiles, win_tile)
     return str(result.han) + ' ' + str(result.fu)
 
-@app.route('/mahjongCard', methods = ['POST'])
+@app.route('/analysisMahjongCard', methods = ['POST'])
 def mahjongCard():
     mahjongSet = request.get_json();
-    return jsonify(mahjongSet);
+
+    tiles = TilesConverter.string_to_136_array(man=mahjongSet['man'], pin=mahjongSet['pin'], sou=mahjongSet['sou'], honors=mahjongSet['honors']);
+
+    winTileInfo = mahjongSet['winTile']
+
+    if winTileInfo['type'] == "man":
+        winTile = TilesConverter.string_to_136_array(man = winTileInfo['value'])[0]
+    elif winTileInfo['type'] == "pin":
+        winTile = TilesConverter.string_to_136_array(pin = winTileInfo['value'])[0]
+    elif winTileInfo['type'] == "sou":
+        winTile = TilesConverter.string_to_136_array(sou = winTileInfo['value'])[0]
+    elif winTileInfo['type'] == "honors":
+        winTile = TilesConverter.string_to_136_array(honors = winTileInfo['value'])[0]
+    
+    result = calculator.estimate_hand_value(tiles, winTile)
+
+    return jsonify({
+        "han":result.han,
+        "fu":result.fu,
+        "cost":result.cost['main'],
+        "yaku":str(result.yaku)
+    });
 
 
 if __name__ == '__main__':
